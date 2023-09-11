ae76a5460c750332d33f0fdd1934fb43b529a9cf
stndpprft <143787766+stndpprft@users.noreply.github.com>
2023-09-11 18:02:56 +0900

    test.py

---
+++ test.py
@@ -0,0 +1,237 @@
+# my_set = {1, 2, 3, 4, 5}
+# setItem = {5, 3, 1}
+# print(my_set)
+
+
+# my_set = {5, 8, 3, 7, 1, "h"}
+
+# print(my_set)
+# print(*my_set)
+
+# my_set = {5, 8, 3, 7, 1, "h"}
+# set_tmp = set("hello")
+
+# print(set_tmp)
+
+# 합집합
+# my_set = {5, 8, 3, 7, 1, "h"}
+# set_item = {7, 8, 4, 2, "h"}
+
+# print(my_set | set_item)
+# print(my_set.union(set_item))
+
+# 차집합
+# my_set = {5, 8, 3, 7, 1, "h"}
+# set_item = {7, 8, 4, 2, "h"}
+
+# print(my_set | set_item)
+
+# print(my_set - set_item)
+
+# print(my_set.difference(set_item))
+
+# 교집합
+# my_set = {5, 8, 3, 7, 1, "h"}
+# set_item = {7, 8, 4, 2, "h"}
+
+# print(my_set | set_item)
+
+# # print(my_set & set_item)
+
+# print(my_set.intersection(set_item))
+
+# 추가
+# my_set = {5, 8, 3, 7, 1, "h"}
+
+# print(my_set)
+
+# my_set.add(9)
+
+# print(my_set)
+
+# my_set = {5, 8, 3, 7, 1, "h"}
+
+# print(my_set)
+
+# my_set.update([5, 9, 7, 4])
+
+# print(my_set)
+
+# 삭제
+# my_set = {5, 8, 3, 7, 1, "h"}
+
+# # my_set.clear()
+# my_set.remove(7)
+
+# # my_set.discard(2)
+# print(my_set)
+
+# my_set = {5, 8, 3, 7, 1, "h"}
+# set_item = {5, 8, 3, 7, 1, "h"}
+
+# print(my_set)
+
+# my_set.difference_update(set_item)
+# print(my_set)
+
+# 타입 변환
+# my_int = 10
+# my_str = str(my_int)
+
+# print(my_int)
+
+# 기본연산처리
+# my_int = 10
+# print(my_int)
+
+# print(my_int + 10)
+
+# 변환 진행
+# my_int = 10
+# print(my_int)
+
+# print(my_int + 10)
+# my_str = str(my_int)
+
+# print(my_str)
+
+# 변환 값 연산
+# my_int = 10
+# print(my_int)
+
+# print(my_int + 10)
+# my_str = str(my_int)
+
+# print(my_str)
+
+# print(my_str + 10)
+
+# my_int = 10
+# print(my_int)
+
+# print(my_int + 10)
+# my_str = str(my_int)
+
+# print(my_str)
+
+# print(my_str + "hello")
+
+# my_str = '10'
+# my_int = int(my_str)
+
+# print(my_int)
+
+# 변환확인
+# 기본 연산
+# my_str = '10'
+# print(my_str)
+
+# my_int = int(my_str)
+# print(my_int)
+
+# 변환 값 연산
+# my_str = '10'
+# print(my_str)
+
+# my_int = int(my_str)
+# print(my_int)
+
+# print(my_int + 10)
+
+# 다른 변환 예시
+# my_str = '10'
+# print(my_str)
+
+# my_int = int(my_str)
+# print(my_int)
+
+# print(my_int + 10)
+
+# my_int2 = int("ten")
+# print(my_int2)
+
+# 산술연산자
+# print(x + y)    
+# print(x - y)    
+# print(x * y)    
+# print(x / y)    
+# print(x % y)    
+# print(x // y)   
+# print(x ** y)   
+
+# 사칙연산
+# a = 10
+# b = 3
+# print(10 + 3)
+# print(10 - 3)
+# print(10 * 3)
+# print(10 / 3)
+# print(10 % 3)
+# print(10 // 3)   
+# print(10 ** 3)   
+
+# 할당 연산자 or 연산자 축약
+# a = 2
+# a = 2
+# a += 2
+# a -= 2
+# a *= 2 
+# a /= 2
+# a **= 2
+
+# # 비교, 관계 연산자
+# a = 10
+# b = 4
+
+# print(10 > 4)    
+# print(10 < 4)    
+# print(10 >= 4)   
+# print(10 <= 4)   
+# print(10 == 4)   
+# print(10 != 4)   
+
+# 논리 연산자
+# a = 1
+# b = 0
+
+# print(a and b)
+# print(a or b)
+# print(not b)
+
+# x = True
+# y = False
+
+# print(x and y)
+# print(x or y)
+# print(not x)
+# print(not y)
+
+# 비트연산자
+# a = 10
+# b = 3
+
+# c = a & b
+# print(c)
+
+# c = a | b
+# print(c)
+
+# c = a ^ b
+# print(c)
+
+# c = ~a
+# print(c)
+
+# c = a << 2
+# print(c)
+
+#멤버 연산자
+# my_list = [9, 4, 3, 7, 8, 'hi']
+# print(4 in my_list)
+
+# print(2 in my_list)
+# print (2 not in my_list)
+
+# my_dic = {"key1" : "v1", "k2" : "v2"}
+# print("k2" in my_dic)
+# print("k2" not in my_dic)
